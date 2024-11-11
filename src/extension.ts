bc067f9e7c4e165430fe3dfdbc150a35474d2b2d
Longhanks <andi.schulz@me.com>
2024-11-11 14:45:46 +0000

    Add icon to toggle explorer autoreveal

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,21 @@
 import * as vscode from 'vscode';
 
+function toggleExplorerAutoReveal() {
+	const settingKey = 'explorer.autoReveal';
+	const config = vscode.workspace.getConfiguration();
+	const currentValue = config.get<boolean>(settingKey);
+	const newValue = !currentValue;
+	config.update(settingKey, newValue, vscode.ConfigurationTarget.Global);
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('vscode-locate-current-file.locateCurrentFile', () => {
+	const disposable1 = vscode.commands.registerCommand('vscode-locate-current-file.locateCurrentFile', () => {
 		vscode.commands.executeCommand("workbench.files.action.showActiveFileInExplorer");
 	});
 
-	context.subscriptions.push(disposable);
+	const disposable2 = vscode.commands.registerCommand('vscode-locate-current-file.toggleExplorerAutoReveal', toggleExplorerAutoReveal);
+
+	context.subscriptions.push(disposable1, disposable2);
 }
 
-export function deactivate() {}
+export function deactivate() { }
